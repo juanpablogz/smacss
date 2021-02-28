ee6da1099655cb3fe50f4220780e627d02cbb273
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-02-28 12:44:33 -0500

    smacss

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="l-container">
+		<div class="card"> 
+      <a href="" class="card__thumbnail"> 
+         <img class="card__img" src="@/assets/img/1.jpg" alt="">
+      </a>
+      <div class="card__text">
+          <h3 class="card__title">Lorem ipsum dolor, sit amet consectetur </h3>
+          <a href="#" class="card__link">Lorem ipsum dolor, sit amet consectetur </a>
+          <p class="card__content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, excepturi! Sed maxime accusantium harum hic aliquid dolorum nostrum quasi asperiores quas officiis illo impedit blanditiis dolores vel, corrupti pariatur dicta.</p>
+          
+          <a href="" role="button" class="button button--absolute"> 
+            <span>Ver Más</span> 
+            <i class="button_icon fas fa-arrow-right"></i>
+          </a>
+      </div>
+    </div>
+
+    <div class="frase"> 
+      <h2 class="frase__texto">lorem ipsum dolor lorem</h2>
+      <a href="" class="button" role="button">
+        <span>Comprar</span>
+        <i class="button_icon fas fa-shopping-cart"></i>
+      </a>
+    </div>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
